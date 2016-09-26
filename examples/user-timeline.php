696b2d20dfe8c95a1b953ad6fd6100085785ddcf
Pieter Hordijk <info@pieterhordijk.com>
2016-09-26 21:34:57 +0200

    Implemented user timeline

---
+++ examples/user-timeline.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\AsyncTwitter\Examples;
+
+use Amp\Artax\Client;
+use PeeHaa\AsyncTwitter\Api\Status\UserTimeline;
+use PeeHaa\AsyncTwitter\Credentials\AccessToken;
+use PeeHaa\AsyncTwitter\Credentials\Application;
+use PeeHaa\AsyncTwitter\Http\Artax;
+use PeeHaa\AsyncTwitter\Api\Client as ApiClient;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$applicationCredentials = new Application(
+    'consumerkey',
+    'consumersecret'
+);
+
+$accessToken = new AccessToken(
+    'accesstoken',
+    'tokensecret'
+);
+
+$httpClient = new Artax(new Client());
+$apiClient  = new ApiClient($httpClient, $applicationCredentials, $accessToken);
+
+$request = (new UserTimeline())
+    ->screenName('EleventyJeeves')
+    ->amount(10)
+    ->minimumId(12345)
+    ->maximumId(780075575276863488)
+    ->excludeReplies()
+    ->includeContributorDetails()
+    ->excludeRetweets()
+    ->trimUser()
+;
+
+\Amp\wait($result = $apiClient->request($request));
