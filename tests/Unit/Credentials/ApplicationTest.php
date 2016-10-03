71ae630129ee6ae0d3e9ff7d71b8606ad6f30cfd
Pieter Hordijk <info@pieterhordijk.com>
2016-10-03 13:32:57 +0200

    Added credentials tests

---
+++ tests/Unit/Credentials/ApplicationTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\AsyncTwitter\Credentials;
+
+use PHPUnit\Framework\TestCase;
+
+class ApplicationTest extends TestCase
+{
+    private $application;
+
+    public function setUp()
+    {
+        $this->application = new Application('TheKey', 'TheSecret');
+    }
+
+    public function testGetKey()
+    {
+        $this->assertSame('TheKey', $this->application->getKey());
+    }
+
+    public function testGetSecret()
+    {
+        $this->assertSame('TheSecret', $this->application->getSecret());
+    }
+}
