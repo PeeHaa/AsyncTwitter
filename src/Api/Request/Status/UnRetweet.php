d137b654e31ad259599824f6f49483117333cdca
Pieter Hordijk <info@pieterhordijk.com>
2016-10-20 22:59:38 +0200

    Added unretweet request

---
+++ src/Api/Request/Status/UnRetweet.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\AsyncTwitter\Api\Request\Status;
+
+use PeeHaa\AsyncTwitter\Api\Request\BaseRequest;
+
+class UnRetweet extends BaseRequest
+{
+    const METHOD   = 'POST';
+    const ENDPOINT = '/statuses/unretweet/%s.json';
+
+    public function __construct(int $id)
+    {
+        parent::__construct(self::METHOD, sprintf(self::ENDPOINT, $id));
+    }
+
+    public function trimUser(): UnRetweet
+    {
+        $this->parameters['trim_user'] = 'true';
+
+        return $this;
+    }
+}
