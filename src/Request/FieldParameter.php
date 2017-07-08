b0f4c5a90a89a7ee6ee5c42d9b68eba0c962c739
Pieter Hordijk <info@pieterhordijk.com>
2017-07-08 16:35:42 +0200

    Defined missing value property

--- src/Request/FieldParameter.php
+++ src/Request/FieldParameter.php
@@ -4,6 +4,8 @@ namespace PeeHaa\AsyncTwitter\Request;
 
 class FieldParameter extends Parameter
 {
+    private $value;
+
     public function __construct(string $key, string $value, string $type = 'text/plain')
     {
         parent::__construct($key, $type);
